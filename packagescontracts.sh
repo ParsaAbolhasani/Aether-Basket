19bf9776885c571548454eddca8807d05b51cd42
Parsa.Rad <87539056+ParsaAbolhasani@users.noreply.github.com>
2026-06-11 01:48:43 +0330

    Add files via upload

---
+++ packagescontracts.sh
@@ -0,0 +1,5 @@
+cd packages/contracts
+forge init --no-commit
+forge install OpenZeppelin/openzeppelin-contracts --no-commit
+forge install smartcontractkit/chainlink-brownie-contracts --no-commit
+forge install foundry-rs/forge-std --no-commit
